5d6eb748f4d834dbb5ea5f47636323235a778cd0
pmudassir <pmuddasir@gmail.com>
2023-07-07 19:10:07 +0530

    create prompt & form designed

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Form from '@components/Form'
+import { useState } from 'react'
+
+const CreatePrompt = () => {
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: '',
+    })
+
+    const createPrompt = async (e) => {
+
+    }
+
+    return (
+        <Form 
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
+    )
+}
+
+export default CreatePrompt
