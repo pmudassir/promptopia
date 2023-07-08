b493760e576adfaa81d9cdabf2ac4e87e320df3d
pmudassir <pmuddasir@gmail.com>
2023-07-08 13:24:17 +0530

    prompt posting completed

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,14 @@
 'use client'
 
 import Form from '@components/Form'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 const CreatePrompt = () => {
+    const router = useRouter()
+    const { data: session } = useSession()
+
     const [submitting, setSubmitting] = useState(false)
     const [post, setPost] = useState({
         prompt: '',
@@ -11,11 +16,31 @@ const CreatePrompt = () => {
     })
 
     const createPrompt = async (e) => {
+        e.preventDefault()
+        setSubmitting(true)
 
+        try {
+            const res = await fetch('/api/prompt/new', {
+                method: "POST",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+            if (res.ok) {
+                router.push("/")
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            //this finally block gets executed either way
+            setSubmitting(false)
+        }
     }
 
     return (
-        <Form 
+        <Form
             type="Create"
             post={post}
             setPost={setPost}
